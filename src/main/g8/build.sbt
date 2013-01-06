854698a6cc79b00ca2f7bf0129c49d32be635507
shadaj <shadaj>
2013-01-05 20:33:08 -0800

    First commit

---
+++ src/main/g8/build.sbt
@@ -0,0 +1,5 @@
+name := $name$
+
+version := "0.1"
+
+scalaVersion := "2.10.0"
---
+++ src/main/g8/src/main/scala/$package$/MainActivity.scala
@@ -0,0 +1,36 @@
+package $package$
+
+import $package$.R;
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.view.View
+import scala.List
+
+class MainActivity extends Activity {
+  var items = List[String]("bob")
+  
+  
+  lazy val itemsDisplay = findViewById(R.id.items).asInstanceOf[TextView]
+  lazy val input = findViewById(R.id.input).asInstanceOf[EditText]
+  
+  def refreshItemsDisplay {
+    itemsDisplay.setText(items.mkString("\n"))
+  }
+  
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+  }
+  
+  def addItem(view: View) {
+    val itemToAdd = input.getText.toString
+    
+    items = items :+ itemToAdd
+    
+    refreshItemsDisplay
+  }
+}
